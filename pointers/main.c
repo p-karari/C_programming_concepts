082a019b70f97912c70a80d1db78a31fd8e0b2f1
p-karari <kararikelvin2002@gmail.com>
2023-05-12 22:41:10 +0300

    pointers in C programming language

---
+++ pointers/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*int age = 30;
+    int *pAge;
+    pAge = &age;
+    double gpa = 3.4;
+    double *pGpa;
+    pGpa = &gpa;
+    char grade = 'A';
+    char * pGrade;
+    pGrade = &grade;
+
+    printf("\nage's memory adress is : %x", &age);
+    printf("\nage's memory adress is : %x", &gpa);
+    printf("\nage's memory adress is : %x", &grade);
+    printf("%d", *pAge);*/
+    printf("\n\n\n  I Am Going To Be Successful ... \n  Complete my four Years In kirinyaga University ...\n Then Go to The States for Work");
+
+
+
+    return 0;
+}
