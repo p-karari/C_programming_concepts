3f197a5d1ba110a851197d82d0ce2752225b7ec1
p-karari <kararikelvin2002@gmail.com>
2023-05-13 11:30:06 +0300

    programme to calculate the sum of 5 numbers

---
+++ sum_of_5_no/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int a,b,c,d,e;
+int sum;
+
+printf("Enter the value :");
+scanf("%d",a);
+
+printf("Enter the value :");
+scanf("%d",b);
+
+printf("Enter the value :");
+scanf("%d",c);
+
+printf("Enter the value :");
+scanf("%d",d);
+
+printf("Enter the value :");
+scanf("%d",e);
+
+sum=a+b+c+d+e;
+
+printf("The sum is :",&sum);
+
+
+
+    return 0;
+}
