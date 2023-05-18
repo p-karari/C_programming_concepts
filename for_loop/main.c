57a3533d3ee25cf032c20967d7a9a08d8372be48
p-karari <kararikelvin2002@gmail.com>
2023-05-18 22:33:01 +0300

    for loop in c

---
+++ for_loop/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*int i = 1;
+    while(i <= 5)
+    {
+        printf("%d\n", i);
+        i++;
+    }*/
+    int luckynumbers[] = {4 , 5 , 68 , 98 , 758 , 90};
+    int i;
+    for(i = 0; i <= 6 ; i++)
+    {
+     printf("%d\n", luckynumbers[i]);
+    }
+    return 0;
+}
