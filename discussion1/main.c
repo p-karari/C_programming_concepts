c0336af6ed8b2df0f136598924871a05a29009a5
p-karari <kararikelvin2002@gmail.com>
2023-05-20 23:57:40 +0300

    c programming group discussion assignment

---
+++ discussion1/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+    int marks = 70;
+    if (marks > 40)
+    {
+        printf("pass");
+    }
+
+
+    else
+        {
+                 printf("fail");
+
+        }
+
+    return 0;
+}
