5beaf765d1cbdc873646be0bc13df4c7abf144a4
p-karari <kararikelvin2002@gmail.com>
2023-05-31 23:54:48 +0300

    hello world programme in c

---
+++ hello.c/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main()
+{
+
+
+/* local variable definition */
+ int a = 10;
+ /* while loop execution */
+  printf("&a",a);
+ printf("value of a: %d\n", a);
+ a++;
+ }
+
+ return 0;
+ }
