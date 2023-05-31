bf8b2a1ec5fe69a54dd0cc8e10564ca9ff04c395
p-karari <kararikelvin2002@gmail.com>
2023-05-31 23:56:15 +0300

    arrays in c programming language

---
+++ Arrays_In_C/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int Luckynumbers[10];
+    Luckynumbers[1] = 80;
+    printf("%d", Luckynumbers[0]);
+    return 0;
+}
+void sayHi ()
+{
+    printf("hello user");
+}
